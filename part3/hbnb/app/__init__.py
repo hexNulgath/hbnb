feec90139da550d86c890b51ac2dcefe308e22b4
hexNulgath <machadoignacio@hotmail.com>
2024-11-12 11:22:36 -0300

    tested login

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -5,6 +5,7 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
+from app.api.v1.auth import api as login_ns
 from flask_jwt_extended import JWTManager
 
 jwt = JWTManager()
@@ -26,6 +27,8 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(places_ns, path='/api/v1/places')
     # Register the Reviews namespace
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    # Register the Reviews namespace
+    api.add_namespace(login_ns, path='/api/v1/auth/login')
 
     bcrypt.init_app(app)
     jwt.init_app(app)
--- part3/hbnb/app/api/v1/auth.py
+++ part3/hbnb/app/api/v1/auth.py
@@ -1,7 +1,9 @@
 from flask_restx import Namespace, Resource, fields
 from flask_jwt_extended import create_access_token
-from app.services import facade
 from flask_jwt_extended import jwt_required, get_jwt_identity
+from app.services.facade import HBnBFacade
+
+facade = HBnBFacade()
 
 
 api = Namespace('auth', description='Authentication operations')
